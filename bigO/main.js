1ac5a81efa6bc2c6eeb825822c4dcb8928ecc566
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-05-20 14:57:43 +0530

    space-complexity;

--- bigO/main.js
+++ bigO/main.js
@@ -1,35 +1,35 @@
-const nemo = ['nemo'];
+// const nemo = ['nemo'];
 
-const everyone = [
-  'dory',
-  'bruce',
-  'marlin',
-  'nemo',
-  'gill',
-  'bloat',
-  'nigel',
-  'squirt',
-  'darla',
-  'hank'
-];
+// const everyone = [
+//   'dory',
+//   'bruce',
+//   'marlin',
+//   'nemo',
+//   'gill',
+//   'bloat',
+//   'nigel',
+//   'squirt',
+//   'darla',
+//   'hank'
+// ];
 
-const large = new Array(10000).fill('nemo');
+// const large = new Array(10000).fill('nemo');
 
-function findNemo(array) {
-  let t0 = performance.now();
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 'nemo') {
-      console.log('Found Nemo!');
-      break;
-    }
-  }
+// function findNemo(array) {
+//   let t0 = performance.now();
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'nemo') {
+//       console.log('Found Nemo!');
+//       break;
+//     }
+//   }
 
-  let t1 = performance.now();
+//   let t1 = performance.now();
 
-  console.log('Call to findNemo took', t1 - t0);
-}
+//   console.log('Call to findNemo took', t1 - t0);
+// }
 
-findNemo(large); /* O(n) - Linear time*/
+// findNemo(large); /* O(n) - Linear time*/
 
 // const boxes = [0, 1, 2, 3, 4, 5];
 
@@ -39,3 +39,36 @@ findNemo(large); /* O(n) - Linear time*/
 // }
 
 // logFirstTwoBoxes(boxes); /* O(1) - Constant time */
+
+/* Log all pairs of array */
+const boxes = ['a', 'b', 'c', 'd', 'e'];
+
+function logAllPairsOfArray(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      console.log(array[i], array[j]);
+    }
+  }
+}
+
+logAllPairsOfArray(boxes); // O(array.length * array.length) -> O(n * n) -> O(n^2)
+
+/* --------- SPACE COMPLEXITY --------- */
+
+function boo(n) {
+  for (let i = 0; i < n.length; i++) {
+    console.log('boo!');
+  }
+}
+
+boo([1, 2, 3, 4, 5]); // O(1) space
+
+function arrayOfHiNTimes(n) {
+  let hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = 'hi';
+  }
+  return hiArray;
+}
+
+arrayOfHiNTimes(6); // O(n) space
