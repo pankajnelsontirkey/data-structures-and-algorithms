5d55feb8046042b1d5fa8c1c82344e1687fd0ede
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-05-20 13:18:04 +0530

    Optimize findingNemo;

--- bigO/main.js
+++ bigO/main.js
@@ -1,40 +1,41 @@
-// const nemo = ['nemo'];
+const nemo = ['nemo'];
 
-// const everyone = [
-//   'dory',
-//   'bruce',
-//   'marlin',
-//   'nemo',
-//   'gill',
-//   'bloat',
-//   'nigel',
-//   'squirt',
-//   'darla',
-//   'hank'
-// ];
+const everyone = [
+  'dory',
+  'bruce',
+  'marlin',
+  'nemo',
+  'gill',
+  'bloat',
+  'nigel',
+  'squirt',
+  'darla',
+  'hank'
+];
 
-// const large = new Array(10000).fill('nemo');
+const large = new Array(10000).fill('nemo');
 
-// function findNemo(array) {
-//   let t0 = performance.now();
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] === 'nemo') {
-//       console.log('Found Nemo!');
-//     }
-//   }
+function findNemo(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found Nemo!');
+      break;
+    }
+  }
 
-//   let t1 = performance.now();
+  let t1 = performance.now();
 
-//   console.log('Call to findNemo took', t1 - t0);
-// }
-
-// findNemo(large); /* O(n) - Linear time*/
-
-const boxes = [0, 1, 2, 3, 4, 5];
-
-function logFirstTwoBoxes(boxes) {
-  console.log(boxes[0]);
-  console.log(boxes[1]);
+  console.log('Call to findNemo took', t1 - t0);
 }
 
-logFirstTwoBoxes(boxes); /* O(1) - Constant time */
+findNemo(large); /* O(n) - Linear time*/
+
+// const boxes = [0, 1, 2, 3, 4, 5];
+
+// function logFirstTwoBoxes(boxes) {
+//   console.log(boxes[0]);
+//   console.log(boxes[1]);
+// }
+
+// logFirstTwoBoxes(boxes); /* O(1) - Constant time */
