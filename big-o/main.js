152df85c23f037ba9251a3f0828ffd68757d8c9b
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-05-20 12:23:01 +0530

    calculate time to execute function;

--- big-o/main.js
+++ big-o/main.js
@@ -1,11 +1,16 @@
 const nemo = ['nemo'];
 
 function findNemo(array) {
+  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
       console.log('Found Nemo!');
     }
   }
+
+  let t1 = performance.now();
+
+  console.log('Call to findNemo took', t1 - t0);
 }
 
 findNemo(nemo);
